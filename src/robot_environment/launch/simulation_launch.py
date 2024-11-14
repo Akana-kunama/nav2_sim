be2b75fdd3ea2c2e9c37bc854ce236b79a7f2e2d
HenryChan <akana_kim@icloud.com>
2024-11-14 15:16:57 +0800

    11 14 modified urdf file and the robot_display_launch now is able to run differ plugin and imu plugin now

--- src/robot_environment/launch/simulation_launch.py
+++ src/robot_environment/launch/simulation_launch.py
@@ -8,12 +8,16 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     robot_description_pkg = get_package_share_directory('robot_description')
     robot_environment_pkg = get_package_share_directory('robot_environment')
-
+    robot_control_pkg = get_package_share_directory('robot_control')
     # Path to the world file
     world_file = os.path.join(robot_environment_pkg, 'worlds', 'sim_world.world')
 
     # Path to the robot's URDF
     urdf_file = os.path.join(robot_description_pkg, 'urdf', 'MicroROS.urdf')
+
+
+    # Path to the robot control config
+    controller_config_file = os.path.join(robot_control_pkg, 'config', 'robot_controllers.yaml')
 
     # Read the URDF file contents into a string
     with open(urdf_file, 'r') as urdf_file_obj:
@@ -37,7 +41,10 @@ def generate_launch_description():
         Node(
             package='gazebo_ros',
             executable='/usr/bin/gzserver',
-            arguments=[world_file, '--verbose','-s', 'libgazebo_ros_init.so','-s', 'libgazebo_ros_factory.so'],
+            arguments=[world_file, '--verbose',
+                    #    '-s', 'libgazebo_ros_init.so',
+                       '-s', 'libgazebo_ros_factory.so',
+                       '--plugin', 'libgazebo_ros2_control.so'],
             output='screen'
         ),
         # Launch Gazebo client
@@ -46,13 +53,7 @@ def generate_launch_description():
             executable='/usr/bin/gzclient',
             output='screen'
         ),
-        # Publish robot state by directly passing the URDF contents
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            output='screen',
-            parameters=[{'robot_description': robot_description_content}]
-        ),
+
         # Spawn robot in Gazebo
         Node(
             package='gazebo_ros',
@@ -61,4 +62,6 @@ def generate_launch_description():
             # arguments=['-file', urdf_file, '-entity', 'micro4.0'],
             output='screen'
         ),
+
+
     ])
